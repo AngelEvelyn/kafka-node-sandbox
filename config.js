d8e8d7942925b9281286f8f36b25503ef6ac7a5b
gp5yk <25909829+gipsykeys@users.noreply.github.com>
2019-07-19 13:50:50 -0700

    improve code decoupling, add script possibility to pass producers as cli arguments

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    kafka_topic: 'first_topic',
-    kafka_server: 'localhost:2181',
-    message:`I'm aliiiiiive`
-  };
+  kafkaServer: 'localhost:2181',
+  intervalInMs: 100,
+  topics: ['first_topic', 'second_topic', 'second_topic', 'third', 'forth', 'second_message'],
+};
--- producer.js
+++ producer.js
@@ -1,16 +1,15 @@
 const kafka = require('kafka-node');
-const bp = require('body-parser');
 const config = require('./config');
 
-const topics = ['first_topic', 'second_topic', 'second_topic', 'third', 'forth', 'second_message'];
+const cliTopics = process.argv.slice(2);
+const topics = cliTopics.length > 0 ? cliTopics : config.topics;
 
 topics.forEach(topicName => {
   try {
     const Producer = kafka.Producer;
-    const client = new kafka.Client(config.kafka_server);
+    const client = new kafka.Client(config.kafkaServer);
     const producer = new Producer(client, {partitionerType: 2});
-    const kafka_topic = config.kafka_topic;
-    console.log(kafka_topic);
+    console.log(topicName);
     let payloads = [
       {
         topic: topicName,
@@ -23,17 +22,17 @@ topics.forEach(topicName => {
       setInterval(() => {
         producer.send(payloads, (err, data) => {
           if (err) {
-            console.log('[kafka-producer -> '+kafka_topic+']: broker update failed');
+            console.log('[kafka-producer -> '+topicName+']: broker update failed');
           } else {
-            console.log('[kafka-producer -> '+kafka_topic+']: broker update success');
+            console.log('[kafka-producer -> '+topicName+']: broker update success');
           }
         });
-      }, 100)
+      }, config.intervalInMs)
     });
   
     producer.on('error', function(err) {
       console.log(err);
-      console.log('[kafka-producer -> '+kafka_topic+']: connection errored');
+      console.log('[kafka-producer -> '+topicName+']: connection errored');
       throw err;
     });
   }
