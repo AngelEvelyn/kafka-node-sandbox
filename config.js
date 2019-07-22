686d90228c8bdceb7c3bf6b97d7aa7a5f92ec996
gp5yk <25909829+gipsykeys@users.noreply.github.com>
2019-07-21 19:49:38 -0700

    increase default msgs per second to 100

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   kafkaServer: 'localhost:2181',
-  intervalInMs: 100,
+  intervalInMs: 10,
   topics: ['first_topic', 'second_topic', 'second_topic', 'third', 'forth', 'second_message'],
 };
